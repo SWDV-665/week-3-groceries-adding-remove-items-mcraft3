b06eabd5037b440f712b6f4aba3733e1e9770f65
mcraft3 <mcraft3@live.maryville.edu>
2022-03-22 20:59:30 -0400

    Week3 Groceries App: Adds Items and Remove Items
    Refactored Items array indenting.

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -16,20 +16,20 @@ export class HomePage {
     {
       name: "Milk",
       quantity: 2 
-  },
-  {
-    name: "Bread",
-    quantity: 1 
-  },    
-  {
-    name: "Banana",
-    quantity: 3 
-  },
-  {
-    name: "Sugar",
-    quantity: 1 
-  },
-];
+    },
+    {
+      name: "Bread",
+      quantity: 1 
+    },    
+    {
+      name: "Banana",
+      quantity: 3 
+    },
+    {
+      name: "Sugar",
+      quantity: 1 
+    },
+  ];
 
   constructor(
     public navCtrl: NavController,
